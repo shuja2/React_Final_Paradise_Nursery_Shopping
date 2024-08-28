befbaa91b97c00881d8d2a03b78a0bfe75022c4e
Shuja2 <shuja201234@gmail.com>
2024-08-28 19:22:26 +0500

    Total calculation

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,12 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    var total = 0;
+    cart.forEach(plant => {
+      total += parseInt(plant.cost.substring(1)) * plant.quantity;
+
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
