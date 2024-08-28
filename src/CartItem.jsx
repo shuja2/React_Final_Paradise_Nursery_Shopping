640dfdf17570970dabeb43d595db1dc70a636676
shuja <shuja201234>
2024-08-28 12:02:24 -0400

    completed cartitem.jsx except total no ogf items

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -18,23 +18,36 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity:  item.quantity + 1}));
+    console.log(' +1 plant');
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity === 1) {
+        dispatch(removeItem(item.name));
+        console.log('removed 1 plant');
+    } else {
+        dispatch(updateQuantity({ name: item.name, quantity:  item.quantity - 1}));
+        console.log(' -1 plant');
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+    console.log('removed plant');
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseInt(item.cost.substring(1)) * item.quantity;
   };
 
   return (
@@ -62,7 +75,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
